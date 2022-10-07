8d8894529eaa07a8517207e14900ac15e93c930e
Tysyatsky <tysyatsky@gmail.com>
2022-10-07 21:18:29 +0300

    Testcase implemented.

--- src/Manager.java
+++ src/Manager.java
@@ -5,24 +5,35 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Manager {
-
     ArrayList<Car> cars;
     int currentYear;
 
     public Manager() {
         cars = new ArrayList<Car>();
         currentYear = 2022;
-        cars.add(new Car(1, Brand.Audi, "XL900", 1900, Color.Black, 900000, "AK9000DK"));
 
         //(int id, Brand brand, String model,
         // int year, Color color, long prise, String registration)
     }
 
-    public static int intParse(){
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+    public void testCase() {
+        cars.add(new Car(1, Brand.Audi, "XL900", 1900, Color.Black, 9_000000, "AK9000DK"));
+        cars.add(new Car(2, Brand.BMV, "XX800", 1900, Color.Black, 10_000000, "AK9000DK"));
+        cars.add(new Car(3, Brand.Mercedes, "XL900", 1900, Color.Black, 11_000000, "AK9000DK"));
+        cars.add(new Car(4, Brand.Mercedes, "XL900", 1900, Color.Black, 6_000000, "AK9000DK"));
+        cars.add(new Car(5, Brand.Audi, "XL900", 1900, Color.Black, 15_000000, "AK9000DK"));
     }
 
+    public static int intParse(){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextInt();
+        }
+        catch(NullPointerException exception){
+            System.out.println(exception.getMessage());
+            throw exception;
+        }
+    }
 
     public static void displayByBrand(){
         //brand
